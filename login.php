0de3351865191ebb4ceba37c3c1677b5bca21e36
bastienrb <bastien.robert@wanadoo.fr>
2016-09-13 18:44:58 +0200

    Started decryption

--- login.php
+++ login.php
@@ -14,8 +14,8 @@ require('model/functions.fn.php');
 if(isset($_POST['email']) && isset($_POST['password'])){
 	if(!empty($_POST['email']) && !empty($_POST['password'])){
 
-		userConnection($db, $_POST['email'], $_POST['password']);
-		
+		$decrypt = password_hash($_POST['password'], PASSWORD_DEFAULT);
+		userConnection($db, $_POST['email'], $decrypt);
 		header('Location: dashboard.php');
 
 	}else{
