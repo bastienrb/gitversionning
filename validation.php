8d164f40098905402e056b51c4cd66d3bbaf10ce
bastienrb <bastien.robert@wanadoo.fr>
2016-09-13 16:08:40 +0200

    add user taken or email taken error

--- validation.php
+++ validation.php
@@ -3,13 +3,24 @@ require('config/config.php');
 require('model/functions.fn.php');
 session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
+$MailCheck = isEmailAvailable($db, $_POST['email']);
+$NameCheck = isUsernameAvailable($db, $_POST['username']);
+
+
+if ((!$MailCheck) || (!$NameCheck)){
+	$_SESSION['message'] = "Votre nom d'utilisateur ou votre email est déjà utilisé";
+	header('Location: register.php');
+}
+
+elseif(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
 	userRegistration($db, $_POST['username'], $_POST['email'], $_POST['password']);
 	header('Location: dashboard.php');
 
-}else{ 
+}
+
+else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
 }
