2ed13d9e665a19832e2fec44906f94937ebdb874
Roland Vrignon <roland.vrignon@gmail.com>
2016-09-16 15:23:44 +0200

    design for premium view

--- view/premium.php
+++ view/premium.php
@@ -1,12 +1,21 @@
-<<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
 <body>
+	<?php include '_topbar.php'; ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 midway-horizontal midway-vertical fadeInDown animated">
+				<div id="musicfeed">
+					<h1><i class="fa fa-pencil"></i> Passer à Soundcloud Premium</h1>
+					<div class="block animated fadeInDown">
+						<div class="row">
+							
+								<form action="#" method="POST">
+									<div class="form-group">
+										<label for="title">Soundcloud</label>
 
-<h1><i class="fa fa-soundcloud"></i> Connectez-vous !</h1>
-				<form method="POST" action="login.php">
+									</div>
+									
+									<div id="logbox">
+									<form method="POST" action="login.php">
 					<div class="form-input">
 						<span class="email">
 							<input type="text" name="email" placeholder="Email">
@@ -22,6 +31,14 @@
 					</div>
 					<p class="account">Vous n'avez pas de compte ? <a href="register.php">Inscrivez vous !</a></p>
 				</form>
-</body>
-</html>
-
+				</div>
+				</div>
+									<p class="clearfix"><button type="submit" class="valid pull-right"><i class="fa fa-check"></i> Valider</button></p>
+								</form>
+							
+						</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
