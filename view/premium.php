39d854b7183777ad0247a72bd3db0b2d17ec8059
Roland Vrignon <roland.vrignon@gmail.com>
2016-09-16 15:10:09 +0200

    added premium in view

---
+++ view/premium.php
@@ -0,0 +1,27 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h1><i class="fa fa-soundcloud"></i> Connectez-vous !</h1>
+				<form method="POST" action="login.php">
+					<div class="form-input">
+						<span class="email">
+							<input type="text" name="email" placeholder="Email">
+						</span>
+					</div>
+					<div class="form-input">
+						<span class="password">
+							<input type="password" name="password" placeholder="Password">
+						</span>
+					</div>
+					<div class="form-submit">
+						<input type="submit" value="Connecter">	
+					</div>
+					<p class="account">Vous n'avez pas de compte ? <a href="register.php">Inscrivez vous !</a></p>
+				</form>
+</body>
+</html>
+
