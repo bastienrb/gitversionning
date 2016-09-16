dcadce76971dea1249ad08ab4008fdb6af56f9e4
Roland Vrignon <roland.vrignon@gmail.com>
2016-09-16 15:35:10 +0200

    final design for premium page

--- view/premium.php
+++ view/premium.php
@@ -4,40 +4,39 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 midway-horizontal midway-vertical fadeInDown animated">
 				<div id="musicfeed">
-					<h1><i class="fa fa-pencil"></i> Passer à Soundcloud Premium</h1>
-						<div class="row">
+					<div class="row">
+						
+						<form action="#" method="POST">
+							<div class="form-group">
+								<label for="title">Soundcloud</label>
+							</div>
 							
-								<form action="#" method="POST">
-									<div class="form-group">
-										<label for="title">Soundcloud</label>
-
+							<div id="logbox">
+								<h1>PASSER PREMIUM</h1>
+								<form method="POST" action="login.php">
+									<div class="form-input">
+										<span class="email">
+											<input type="text" name="email" placeholder="Email">
+										</span>
 									</div>
-									
-									<div id="logbox">
-									<form method="POST" action="login.php">
-					<div class="form-input">
-						<span class="email">
-							<input type="text" name="email" placeholder="Email">
-						</span>
-					</div>
-					<div class="form-input">
-						<span class="password">
-							<input type="password" name="password" placeholder="Password">
-						</span>
-					</div>
-					<div class="form-submit">
-						<input type="submit" value="Connecter">	
-					</div>
-					<p class="account">Vous n'avez pas de compte ? <a href="register.php">Inscrivez vous !</a></p>
-				</form>
-				</div>
-				</div>
-									<p class="clearfix"><button type="submit" class="valid pull-right"><i class="fa fa-check"></i> Valider</button></p>
+									<div class="form-input">
+										<span class="password">
+											<input type="password" name="password" placeholder="Password">
+										</span>
+									</div>
+									<div class="form-submit">
+										<input type="submit" value="Connecter">
+									</div>
+									<p class="account">Vous n'avez pas de compte ? <a href="register.php">Inscrivez vous !</a></p>
 								</form>
-							
+							</div>
 						</div>
+						
+						
+						
+					</div>
 					
 				</div>
 			</div>
 		</div>
-	</div>
+	
