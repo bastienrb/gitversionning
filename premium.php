810122a0232172f92a3be2674a319a8c4d329947
Roland Vrignon <roland.vrignon@gmail.com>
2016-09-16 15:23:17 +0200

    setup controller for premium

--- premium.php
+++ premium.php
@@ -3,8 +3,9 @@ session_start();
 require('config/config.php');
 require('model/functions.fn.php');
 
+
+
 include 'view/_header.php';
-include 'view/_topbar.php';
-include 'view/_footer.php';
 include 'view/premium.php';
+include 'view/_footer.php';
 ?>
